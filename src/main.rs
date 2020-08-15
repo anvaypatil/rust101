0595716b7d075bef1c3952ecbbbfae1b327c4da6
Anvay P <anvay13p@gmail.com>
2020-08-15 14:43:27 +0530

    commit notes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+// mod leetcode;
 fn main() {
-    let s = String::from("abc");
-    let b = s.clone();
-    println!("{}", s);
-    println!("{}", b);
+    // leetcode::arrays::l1_two_sum::execute();
+    partial();
+}
+
+fn partial() {
+    let v = vec![4, 5, 6, 7, 4, 5, 6, 4, 4, 3, 3, 5, 5, 3, 4, 5, 5, 6, 3];
+    for i in &v {
+        let r: i32 = count(&v, i);
+        println!("{} -> {}", , String::from(r) );
+    }
+}
+
+fn count(v: &Vec<i32>, val: &i32) -> i32 {
+    v.into_iter().filter(|&&x| x == *val).count() as i32
 }
